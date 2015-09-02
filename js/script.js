12633c6557dbd2689d50608770ec7691eb2fb7b4
James Vidal Guzmán <james.vidal@altimea.com>
2015-09-02 14:59:33 -0500

    Responsive
    beta 0.0

--- js/script.js
+++ js/script.js
@@ -13,22 +13,41 @@ $(document).ready(function() {
 		$(".controles a.seccion1").removeClass('active');
 		$(".ancla-area.col2").click();
 	});
-	$('.controles').delegate('a.secthree','click',function(e){
+	$('.controles').delegate('a.sectree','click',function(e){
 		$(".controles a.seccion1").removeClass('active');
 		$(".ancla-area.col3").click();
 	});
+	//Buton plus
+	$(".sc1 .mroption .button").click(function() {
+		$(this).toggleClass("active");
+		$(".ancla-area.col1").click();
+	});
+	$(".sc2 .mroption .button").click(function() {
+		$(this).toggleClass("active");
+		$(".ancla-area.col2").click();
+	});
+	$(".sc3 .mroption .button").click(function() {
+		$(this).toggleClass("active");
+		$(".ancla-area.col3").click();
+	});
 	//Anclas dinamicas
+
+	//Ancla 1
 	$(".ancla-area.col1").click(function(){
 		if( $("li.sc1.st-open").length < 1 ){
+			$(".sc1 .mroption .button").removeClass('active');
 			$(".controles a.seccion1").removeClass('active secone');
 		}
 		if( $("li.sc1.st-open").length > 0 ){
+			$(".sc1 .mroption .button").addClass('active');
 			$(".controles a.seccion1").addClass('active secone');
-			$(".controles a.seccion2").removeClass('active');
+			$(".controles a.seccion2, .controles a.seccion3").removeClass('active');
+			$(".controles a.seccion1").removeClass('sectree');
+			$(".controles a.seccion1").removeClass('sectwo');
 		}
 	});
 
-
+	//Ancla 2
 	$(".controles a.seccion2").click(function(){
 		$(".controles a.seccion2").removeClass('active');
 		$(".ancla-area.col2").click();
@@ -36,17 +55,21 @@ $(document).ready(function() {
 	});
 	$(".ancla-area.col2").click(function(){
 		if( $("li.sc2.st-open").length < 1 ){
+			$(".sc2 .mroption .button").removeClass('active');
 			$(".controles a.seccion2").removeClass('active');
 			$(".controles a.seccion1").removeClass('active sectwo');
 		}
 		if( $("li.sc2.st-open").length > 0 ){
+			$(".sc2 .mroption .button").addClass('active');
 			$(".controles a.seccion2").addClass('active');
 			$(".controles a.seccion1").addClass('active sectwo');
 			$(".controles a.seccion1").removeClass('secone');
+			$(".controles a.seccion1").removeClass('sectree');
+			$(".controles a.seccion3").removeClass('active');
 		}
 	});
 
-
+	//Ancla 3
 	$(".controles a.seccion3").click(function(){
 		$(".controles a.seccion3").removeClass('active');
 		$(".ancla-area.col3").click();
@@ -54,15 +77,18 @@ $(document).ready(function() {
 	});
 	$(".ancla-area.col3").click(function(){
 		if( $("li.sc3.st-open").length < 1 ){
+			$(".sc3 .mroption .button").removeClass('active');
 			$(".controles a.seccion3").removeClass('active');
-			$(".controles a.seccion3").removeClass('active');
+			$(".controles a.seccion2").removeClass('active');
 			$(".controles a.seccion1").removeClass('active sectree');
 
 			$('audio,video').each(function(){ this.pause(); });
 		}
 		if( $("li.sc3.st-open").length > 0 ){
+			$(".sc3 .mroption .button").addClass('active');
 			$(".controles a.seccion3").addClass('active');
 			$(".controles a.seccion1").addClass('active sectree');
+			$(".controles a.seccion1").removeClass('secone');
 			$(".controles a.seccion1").removeClass('sectwo');
 			$(".controles a.seccion2").removeClass('active');
 		}
